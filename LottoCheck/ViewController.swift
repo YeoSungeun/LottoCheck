558249405b56fea5e8666d5b767ed7215f0f55c4
YeoSungeun <dutjddms@naver.com>
2024-06-05 23:01:20 +0900

    Initial Commit

---
+++ LottoCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoCheck
+//
+//  Created by 여성은 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
